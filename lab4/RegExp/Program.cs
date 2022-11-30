9f93746e5a765785e6fc9b87dd53979c8d9ac9d9
OLagsV <99918627+OLagsV@users.noreply.github.com>
2022-11-30 20:49:14 +0300

    Add files via upload

---
+++ lab4/RegExp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mainString = "дом шар гора дорога дом дорога квадрат машина овал земля";
+            string wordToChange = "дом";
+            string newWord = "ТАРЕЛКА";
+            Console.WriteLine(mainString);
+            Console.WriteLine(mainString.Replace(wordToChange, newWord));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab4/lab42regexp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace lab42regexp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchPhone = @"[+][0-9]{7}";
+            Console.Write("Введите предложение: ");
+            string consoleEnter = Console.ReadLine();
+            StringBuilder stringToCheck = new StringBuilder();
+            stringToCheck.AppendLine(Regex.Replace(consoleEnter, searchPhone, ""));
+            Console.WriteLine(stringToCheck);
+            Console.ReadLine();
+        }
+    }
+}
